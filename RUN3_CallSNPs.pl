78288159815cbd7ec2e80f0545a689d77ec2d705
hpriest <hdpriest@gmail.com>
2015-01-17 19:21:27 +0000

    Major changes, added some more parsing, handling for multiallelic locations. have not yet added overlap support

---
+++ RUN3_CallSNPs.pl
@@ -0,0 +1,239 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use threads;
+use Thread::Queue;
+use FindBin;
+use lib "$FindBin::Bin/Lib";
+use Configuration;
+use Tools;
+
+my $configFile=$ARGV[0];
+
+die "usage : perl $0 <config file governing all alignment>\n\n" unless $#ARGV==0;
+
+my $q = Thread::Queue->new();
+my $config = Configuration->new($configFile);
+my $threads = $config->get("OPTIONS","ManagerThreads");
+my @Groups = $config->getAll("GROUPS");
+my $mchScriptDir = $FindBin::Bin."/Lib/SysCall_1_1/";
+my $mchScript=$mchScriptDir."/SysCall.pl";
+for(my $i=0;$i<=$#Groups;$i++){
+	warn "enqueuing $i ($Groups[$i])\n";
+	$q->enqueue($Groups[$i]);
+}
+
+for(my$i=0;$i<$threads;$i++){
+	my $thr=threads->create(\&workerThread);
+}
+while(threads->list()>0){
+	my @thr=threads->list();
+	$thr[0]->join();
+}
+
+
+sub workerThread{
+	while(my $work=$q->dequeue_nb()){
+		my $grp		= $work;
+		my $DataDir 	= $config->get("DIRECTORIES","Output")."/".$grp;
+		my $TempDir	= $config->get("DIRECTORIES","Temp");
+		my $OutDir  	= $config->get("DIRECTORIES","Output")."/".$grp;
+		my $RefDir	= $config->get("DIRECTORIES","References");
+		checkDir($DataDir,$TempDir,$OutDir,$RefDir);
+		my $workThreads = $config->get("OPTIONS","BWAThreads");
+		my $bwa		= $config->get("PATHS","bwa");
+		my $samtools	= $config->get("PATHS","samtools");
+		my $bcftools	= $config->get("PATHS","bcftools");
+		my $snpRate	= $config->get("OPTIONS","snpRate");
+		my $minCov	= $config->get("OPTIONS","minCov");
+		my $parser	= $FindBin::Bin."/Scripts/".$config->get("PATHS","Parser");
+		my @CurrentSourcePaths;
+		my @GarbageCollector;
+
+		my $file1=$DataDir."/".$grp.".R1.fastq";
+		my $file2=$DataDir."/".$grp.".R2.fastq";
+	
+		die "Cannot find read 1 for group: $grp\nFile missing: $file1\nexiting...\n" unless -e $file1;
+		die "Cannot find read 2 for group: $grp\nFile missing: $file2\nexiting...\n" unless -e $file2;
+		
+		my @Indicies 	= split(",",$config->get("GROUPS",$grp));
+		foreach my $index (@Indicies){
+			my $IndexPath=$RefDir."/".$index;
+			my $alias=$index;
+			my $baseOutput=$OutDir."/$grp.vs.$index.Alignments";
+#			my $baseOutput = $TempDir."/".$grp."_vs_".$alias;
+			my $finalOutput = $OutDir."/".$grp.".vs.".$index;
+			$alias =~ s/\..+//;
+			my $command = "$samtools mpileup -F 0.00001 -g -C50 -d 10000000 -f $IndexPath $baseOutput.sorted.bam | $bcftools view -b -m 0.01 -p .99 - | $bcftools view - > $baseOutput.raw.vcf";
+			warn $command."\n";
+			`$command`;
+			my $csvOut = $config->get("DIRECTORIES","Output")."/".$grp.".csv";
+			$command = "perl $parser $baseOutput.raw.vcf $snpRate $minCov $csvOut";
+			`$command`;
+#			my %H = %{parseResults("$baseOutput.raw.vcf",$baseOutput.".filt.vcf",$snpRate,$minCov)};
+#			push @GarbageCollector, $baseOutput.".filt.vcf";
+#			push @GarbageCollector, $baseOutput.".fasta.raw.vcf";
+#			my $outFinal = $baseOutput.".final.vcf";
+#			my @outFinal;			
+#			foreach my $key (keys %H){
+#				my $K=$H{$key}{"Chr"}."-".$H{$key}{"Pos"};
+#				push @outFinal, $H{$key}{"L"};
+#			}
+#			Tools->printToFile($outFinal,\@outFinal);
+		}
+		collectTheGarbage(@GarbageCollector);
+	}
+}
+
+sub parseToFinal {
+	my $line=shift;
+	my @line=split(/\t/,$line);
+	my $chr=$line[0];
+	my $pos=$line[1];
+	my $refBase=$line[3];
+	next if $line[4] eq "X";
+        my @posAlt =split(/\,/,$line[4]);
+	my %I=%{parseInfo($line[7])};
+			
+}
+
+sub getSysErrors {
+	my $file=shift;
+	my @file=@{Tools->LoadFile($file.".sys_errors")};
+	my $head=shift @file;
+	my %H;
+	foreach my $line (@file){
+		$line=~s/\s.+//;
+		my ($chr,$coord)=split(/\:/,$line);
+		my $k="$chr-$coord";
+		$H{$k}=1;
+	}
+	return \%H;
+}
+
+sub generateMeacham {
+	my $file=shift;
+	my $out=shift;
+	my %H=%{$_[0]};
+	my @output;
+	my %Fasta=%{Tools->LoadFasta($file)};
+	foreach my $pos (keys %H){
+		my $chr=$Fasta{$H{$pos}{"Chr"}};
+		next if $pos <3;
+		my $rpos=$pos-3;  ### -1 for coordinate correct, -2 for start of meacham.
+		my $seq=substr($chr,$rpos,5);
+		my @seq=split(//,$seq);
+		push @output, $H{$pos}{"Chr"}." ".$pos." ".join(" ",@seq);
+	}
+	return 0 unless scalar(@output)>0;
+	Tools->printToFile($out,\@output);
+	return 1;
+}
+
+sub checkDir {
+	foreach my $dir (@_){
+		die "Cannot find directory: $dir\n" unless -e $dir;
+	}
+	return 1;
+}
+
+sub parseResults {
+	my $file=shift;
+	my $output=shift;
+	my $rate=shift;
+	my $minCov=shift;
+	my @file=@{Tools->LoadFile($file)};
+	my @out;
+	my %R;
+	foreach my $line (@file){
+		my @line=split(/\t/,$line);
+		my $chr=$line[0];
+		my $pos=$line[1];
+		my $refBase=$line[3];
+		next if $line=~m/^\#/;
+		next if $line[4] eq "X";
+		next if $line =~m/INDEL/;
+		my @posAlt =split(/\,/,$line[4]);
+		my %I=%{parseInfo($line[7])};
+		unless(defined $I{"DP"}){
+			warn $line."\n";
+			die "malformed output from SNP calling!\n";
+		}
+		unless(defined $I{"I16"}){
+			warn $line."\n";
+			die "malformed output from SNP calling!\n"
+		}
+		unless( defined $I{"QS"}){
+			warn $line."\n";
+			die "malformed output from SNP calling!\n";
+		}
+		my %r;
+		$r{"Chr"}=$chr;
+		$r{"Pos"}=$pos;
+		$r{"DP"}=$I{"DP"};
+		$r{"I16"}=$I{"I16"};
+		$r{"QS"}=$I{"QS"};
+		$r{"L"}=$line;
+		if(checkInfo(\%r,$rate,$minCov)){
+			$R{$pos}=\%r;
+			push @out, $line;
+		}
+	}
+#	Tools->printToFile($output,\@out);
+	return \%R;
+}
+
+sub checkInfo {
+	my %r=%{$_[0]};
+	my $rate=$_[1];
+	my $minCov=$_[2];
+	my @I=split(/\,/,$r{"I16"});
+	my @QS=split(/\,/,$r{"QS"});
+	if($r{"DP"}<$minCov){
+		return 0;
+	}
+	my $s=$I[2]+$I[3];
+#	if (($s/$r{"DP"})>$rate){
+	if ($QS[1]>=0.001){
+		return 1;
+	}
+	return 0;
+}
+
+sub parseInfo {
+	my $info=shift;
+	my @info=split(/\;/,$info);
+	my %h;
+	foreach my $stanza (@info){
+		my ($key,$value)=split(/\=/,$stanza);
+		$h{$key}=$value;
+	}
+	return \%h;
+}
+
+sub collectTheGarbage {
+	my @files = @_;
+	foreach my $file (@files){
+		my $command="rm -rf $file";
+		next unless -e $file;
+		warn $command."\n";
+		`$command`;
+	}
+	return 1;
+}
+
+sub prepFinal {
+	my $finalDir = shift @_;
+	my @files = @_;
+	foreach my $file (@files){
+		my $sPath=$file;
+		my $oPath=$file;
+		$oPath=~s/.+\///g;
+		$oPath=$finalDir."/".$oPath;
+		my $command = "mv $sPath $oPath";
+		warn $command."\n";
+		`$command`;
+	}
+	return 1;
+}
+
---
+++ RUN4_IdentifySNPsByDist.pl
@@ -0,0 +1,202 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use threads;
+use FindBin;
+use Thread::Queue;
+use lib "$FindBin::Bin/Lib";
+use Configuration;
+use Tools;
+
+
+my $usage = "perl $0 <Configuration>\n\n";
+die $usage unless $#ARGV==0;
+
+my $config = Configuration->new($ARGV[0]);
+my @Groups = $config->getAll("GROUPS");
+my $OutBase= $config->get("DIRECTORIES","Output");
+foreach my $grp (@Groups){
+	my @indicies = split(/\,/,$config->get("GROUPS",$grp));
+	foreach my $index (@indicies){
+		my $vcfFile = $OutBase."/".$grp."/$grp.vs.$index.Alignments.raw.vcf";
+		warn "Can't find $vcfFile\n" unless -e $vcfFile;
+		my $outFile = $OutBase."/".$grp."/$grp.byDist.csv";
+		parseForDistribs($vcfFile,$outFile,$config->get("OPTIONS","snpRate"),$config->get("OPTIONS","minZ"));
+	}
+}
+
+sub parseForDistribs {
+	my $in_File=shift;
+	my $outFile=shift;
+	my $Scut = shift;
+	my $Zcut = shift;
+	my @Input = @{Tools->LoadFile($in_File)};
+	my @insertfreqs;
+	my @output;
+	my @OL;
+	my @OC;
+	warn $in_File."\n";
+	my $main=0;
+	foreach my $line (@Input){
+		next if $line=~m/^\#/;
+		next if $line=~m/INDEL/;
+		my %line=%{parseVCF($line)};
+#Notch_3_HC      1514    .       A       G,T,X   0       .       DP=28329;I16=8212,19947,20,37,1023967,37747939,2006,73002,1404317,70080057,2528,112756,471290,10195328,960,20104;QS=0.998061,0.001880,0.000059,0.000000;VDB=3.929859e-01;RPB=-2.551181e-01      PL      0,255,255,255,255,255,255,255,255,255
+#Notch_3_HC      1587    .       C       T,X     0       .       DP=14643;I16=1934,12587,3,15,540693,20391583,623,22221,698795,34197409,802,35920,230168,4534778,265,5131;QS=0.998836,0.001164,0.000000,0.000000;VDB=1.619116e-01;RPB=1.177923e+00       PL      0,255,255,255,255,255
+#Notch_3_HC      206     .       T       C,X     0       .       DP=6211;I16=2797,2859,271,196,200591,7229181,16142,571640,282413,14105661,20858,933236,101978,2266550,8989,199349;QS=0.925546,0.074454,0.000000,0.000000;VDB=2.007198e-01;RPB=-1.554554e-01     PL      0,255,255,255,255,255
+		my @I16=@{$line{I16}};
+		my $tgr = $I16[0]+$I16[1];
+		my $tgnr= $I16[2]+$I16[3];
+		my @Q=@{$line{QS}};
+		my $rq  = shift @Q;
+		my @A=@{$line{alt}};
+		my $pass=0;
+		my $out = $line{chr};
+		$out.=",".$line{pos};
+		$out.=",".$line{ref};
+		$out.=",".$line{DP};
+		$out.=",".$I16[0];
+		$out.=",".$I16[1];
+		$out.=",".$rq;
+		for(my$i=0;$i<=$#Q;$i++){
+			if($Q[$i] >= $Scut){
+				warn $line."\n" unless defined $A[$i];
+#				push @output, $line;
+				#next if $A[$i] eq "X";
+				#warn $line if $line{pos} == 206;
+				$out.=",".$A[$i];
+				$out.=",".$I16[2];
+				$out.=",".$I16[3];
+				$out.=",".$Q[$i];
+				push @output, $out;
+				$main++;
+			}elsif($Q[$i]==0.0){
+			}else{
+				my $minRat = getQRatios($Scut,\@Q);
+#				warn $minRat."\n".$line."\n";
+				my @OD;
+				push @OD, $line{pos};
+				push @OD, $A[$i];
+				push @OD, $Q[$i];
+				push @insertfreqs, $Q[$i];
+				push @OC, \%line;
+				push @OL, \@OD;
+				last;
+			}
+		}
+	}
+	my $mean = Tools->mean(@insertfreqs);
+	my $stdev= Tools->stdev(@insertfreqs);
+	print "mean: $mean\n";
+	print "stdev: $stdev\n";
+	my $hits=0;
+	foreach my $ref (@OC){
+		my %line = %{$ref};
+		my $out = 
+		my @Q = @{$line{QS}};
+		my $rq = shift @Q;
+		my @A=@{$line{alt}};
+		my @I16=@{$line{I16}};
+		for(my$i=0;$i<=$#Q;$i++){
+			next if $Q[$i] >= $Scut;
+			next if $Q[$i] == 0;
+			my $minRat =  getQRatios($Scut,\@Q);
+			my $N = $A[$i];
+			my $Z = ($Q[$i] - $mean)/$stdev;
+#			print "$Q[$i] $mean $stdev equals $Z\n"; 
+			if($Z>$Zcut){
+				#push @output, $line{line}."\t".$Q[$i]."\t".$mean."\t".$stdev."\t".$Z;
+				## ALTERNATIVE Q 1 Q2 etc
+			next if $N eq "X";
+				push @output, $line{chr}.",".$line{pos}.",".$line{ref}.",".$line{DP}.",".$I16[0].",".$I16[1].",".$Q[0].",".$N.",".$I16[2].",".$I16[3].",".$Q[$i].",".$Z.",".$minRat;
+#				push @output, $N;
+				$hits++;
+			}
+		}
+	}
+#	for(my$r=0;$r<=$#OL;$r++){
+	# proc candidate non-zero variance locations
+#		my @D = @{$OL[$r]};
+#		my $pos = shift @D;
+#		for(my$i=0;$i<=$#D-1;$i+=2){
+#			my $N = $D[$i];
+#			my $Q = $D[$i+1];
+#			my $Z = ($Q-$mean)/$stdev;
+#			if($Z>$Zcut){
+#				push @output, $OC[$r]."\t".$Q."\t".$mean."\t".$stdev."\t".$Z;
+#				$hits++;
+#			}
+#		}
+#	}
+	warn "$main locations exceeded base SNP call rate, $hits exceeded one-tail-Z>=2.33 (p<0.01)\n";
+	warn "printing results to $outFile\n";
+	Tools->printToFile($outFile,\@output);
+}
+
+sub getQRatios {
+	my $cut=shift;
+	my @Q=@{$_[0]};
+	my @R;
+	for(my$i=0;$i<=$#Q;$i++){
+		for(my$j=0;$j<=$#Q;$j++){
+			next if $Q[$j] == 0;
+			next if $Q[$i] == 0;
+			next if $Q[$i] > $cut;
+			next if $Q[$j] > $cut;
+			my $rat = $Q[$i]/$Q[$j];
+			push @R, $rat;
+		}
+	}
+	@R = sort {$b <=> $a} @R;
+	return $R[0];
+}
+
+sub parseVCF {
+	my $line=shift;
+	my @line=split(/\t/,$line);
+	my %line;
+	$line{chr}=$line[0];
+	$line{pos}=$line[1];
+	$line{ref}=$line[3];
+	$line{line}=$line;
+#Notch3LC        888     .       T       A,C,X   0       .       DP=7091;I16=1098,5973,0,2,270195,10431055,35,617,299853,14097665,100,5000,25081,194313,3,5;QS=0.999854,0.000079,0.000067,0.000000;VDB=7.840000e-02;RPB=1.093222e+00    PL      0,255,255,255,255,255,255,255,255,255
+#Notch3LC        889     .       A       C,X     0       .       DP=6870;I16=1062,5778,0,1,223630,7461316,13,169,288790,13556532,50,2500,18156,150320,0,0;QS=0.999933,0.000067,0.000000,0.000000;RPB=1.731544e+00       PL      0,255,255,255,255,255
+#Notch3LC        890     .       A       G,X     0       .       DP=3363;I16=1012,2333,1,1,120111,4373929,41,953,117909,5120699,97,4709,14818,117412,2,4;QS=0.999554,0.000446,0.000000,0.000000;VDB=7.680000e-02;RPB=1.522834e+00       PL      0,255,255,255,255,255
+	if($line[4] eq "X"){
+		my @a = ("X");
+		$line{alt}=\@a;
+	}else{
+		$line{alt}=[ split(/\,/,$line[4]) ];
+	}
+	my %info=%{parseInfo($line[7])};
+	$line{DP}=$info{"DP"};
+	$line{I16}=$info{"I16"};
+	$line{QS}=$info{"QS"};
+	return \%line;
+}
+
+sub parseInfo {
+	my $info=shift;
+	my @info=split(/\;/,$info);
+	my %info;
+	foreach my $item (@info){
+		my ($key,$val)=split(/\=/,$item);
+		if($key eq "I16"){
+			$info{$key}=[split(/\,/,$val)];
+		}elsif($key eq "DP"){
+			$info{$key}=$val;
+		}elsif($key eq "QS"){
+			$info{$key}=[split(/\,/,$val)];
+		}else{
+		}
+	}
+	return \%info;
+}
+
+#
+#Notch_3_HC      1514    .       A       G,T,X   0       .       DP=28329;I16=8212,19947,20,37,1023967,37747939,2006,73002,1404317,70080057,2528,112756,471290,10195328,960,20104;QS=0.998061,0.001880,0.000059,0.000000;VDB=3.929859e-01;RPB=-2.551181e-01      PL      0,255,255,255,255,255,255,255,255,255
+#Notch_3_HC      1587    .       C       T,X     0       .       DP=14643;I16=1934,12587,3,15,540693,20391583,623,22221,698795,34197409,802,35920,230168,4534778,265,5131;QS=0.998836,0.001164,0.000000,0.000000;VDB=1.619116e-01;RPB=1.177923e+00       PL      0,255,255,255,255,255
+#Notch_3_HC      206     .       T       C,X     0       .       DP=6211;I16=2797,2859,271,196,200591,7229181,16142,571640,282413,14105661,20858,933236,101978,2266550,8989,199349;QS=0.925546,0.074454,0.000000,0.000000;VDB=2.007198e-01;RPB=-1.554554e-01     PL      0,255,255,255,255,255
+#Notch_3_HC      823     .       C       G,T,A   0       .       DP=22419;I16=13616,8574,18,12,809817,30045033,1070,39182,1105853,55173431,1336,59566,383557,8178791,507,10373;QS=0.998679,0.001048,0.000222,0.000051;VDB=1.024477e-01;RPB=4.940619e-01  PL      0,255,255,255,255,255,255,255,255,255
+#Notch_3_HC      223     .       T       G,C,X   0       .       DP=7297;I16=2775,3660,18,1,188615,6009625,338,6332,319350,15865272,945,47011,101923,2109925,251,4557;QS=0.998211,0.001588,0.000201,0.000000;VDB=1.498251e-01;RPB=4.807658e+00   PL      0,255,255,255,255,255,255,255,255,255
+#
